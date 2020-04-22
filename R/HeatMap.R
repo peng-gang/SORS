266d1fe32ec4e903039557ededaa05f4864cdbdc
gpeng <michael.gang.peng@gmail.com>
2020-04-21 22:22:50 -0400

    add ideogram and heatmap

--- R/HeatMap.R
+++ R/HeatMap.R
@@ -1,2 +1,47 @@
 # heatmap
 
+library(ComplexHeatmap)
+library(circlize)
+
+heatmapSORs <- function(SORs, weight = FALSE, showLegend = TRUE){
+  nSample = ncol(SORs) - 3
+  x <- matrix(0, nrow = nrow(SORs), ncol = nSample)
+  x[SORs[, 4:(nSample+3)] == "DEL"] <- -1
+  x[SORs[, 4:(nSample+3)] == "AMP"] <- 1
+  
+  if(weight){
+    wt <- SORs$End - SORs$Start
+    x <- x*wt
+    x <- x/max(wt)
+  }
+
+  rownames(x) <-  paste(SORs$Chr, SORs$Start, SORs$End, sep = "_")
+  colnames(x) <- colnames(SORs)[4:(nSample+3)]
+  
+  if(weight){
+    col_fun <- colorRamp2(c(-1, -1e-10, 0, 1e-10, 1), c("red", "red", "white", "blue", "blue"))
+  } else {
+    col_fun <- colorRamp2(c(-1, 0, 1), c("red", "white", "blue"))
+  }
+  
+  
+  # ha = HeatmapAnnotation(
+  #   Grade = G,
+  #   Recurrence = R,
+  #   BCLC = BCLC,
+  #   col = list(Grade = c("G2" = "brown1", "G3" = "brown3"),
+  #              Recurrence = c("R" = "darkorange3", "NR" = "darkorange1"),
+  #              BCLC = c("A" = "gray75", "B" = "gray50", "C" = "gray25")))
+  
+  chrOrder <- factor(SORs$Chr, levels = unique(SORs$Chr), ordered = TRUE)
+  hm <- Heatmap(x, name = "CNV",
+                cluster_rows = FALSE, show_row_names = FALSE,
+                col = col_fun,
+                row_split = chrOrder, row_title_rot = 0,
+                row_gap = unit(0, "mm"),  border = TRUE, 
+                how_heatmap_legend = showLegend, 
+                use_raster = FALSE)
+                #top_annotation = ha)
+  draw(hm)
+}
+
--- R/Ideogram.R
+++ R/Ideogram.R
@@ -1,5 +1,6 @@
 # Ideogram
 library(circlize)
+library(karyoploteR)
 
 ideogramCircle <- function(
   SORs, 
@@ -7,8 +8,8 @@ ideogramCircle <- function(
   ylim = NULL, 
   chromosomeIndex = NULL,
   atY = NULL,
-  colorAMP = "blue",
-  colorDEL = "red"){
+  colorAmp = "blue",
+  colorDel = "red"){
   
   nSample = ncol(SORs) - 3
   fryTotal <- rowSums(SORs[,4:(3+nSample)] != "NONE")/nSample
@@ -55,7 +56,7 @@ ideogramCircle <- function(
   circos.genomicTrack(bedFry, ylim = ylim, bg.border = NA,
                       panel.fun = function(region, value, ...) {
                         circos.genomicRect(region, value, ytop.column = 1, ybottom = 0, 
-                                           col = ifelse(value[[1]] > 0, colorAMP, colorDEL),
+                                           col = ifelse(value[[1]] > 0, colorAmp, colorDel),
                                            border = NA, ...)
                         #circos.lines(CELL_META$cell.xlim, c(0, 0), lty = 2, col = "#00000040")
                       })
@@ -66,6 +67,80 @@ ideogramCircle <- function(
 }
 
 
-ideogram <- function(SORs){
+ideogram <- function(
+  SORs,
+  hg = "hg18", 
+  colorAmp = "blue",
+  colorDel = "red",
+  includeAB = TRUE,
+  abAmp = NULL,
+  abDel = NULL
+  ){
+  nSample = ncol(SORs) - 3
+  fryTotal <- rowSums(SORs[,4:(3+nSample)] != "NONE")/nSample
+  fryAmp <- rowSums(SORs[,4:(3+nSample)] == "AMP")/nSample
+  fryDel <- rowSums(SORs[,4:(3+nSample)] == "DEL")/nSample
+  
+  bedFry <- data.frame(
+    chr = rep(SORs$Chr, 2),
+    start = rep(SORs$Start, 2),
+    end = rep(SORs$End, 2),
+    fry = c(fryAmp, -fryDel),
+    stringsAsFactors = FALSE
+  )
+  
+  bedFry$chr <- paste0("chr", bedFry$chr)
+  bedFry <- bedFry[bedFry$fry!=0,]
+  
+  bedAmp <- bedFry[bedFry$fry>0,]
+  bedDel <- bedFry[bedFry$fry<0,]
+  bedDel$fry <- -bedDel$fry
+  
+  if(is.null(abAmp)){
+    if(is.null(abDel)){
+      abAmp <- abDel <- ceiling(max(c(max(bedAmp$fry), max(bedDel$fry))) * 10)/10
+      labelAmp <- labelDel <- paste0(abAmp * 100, "%")
+    } else {
+      abAmp <- abDel
+      labelAmp <- labelDel <- paste0(abAmp * 100, "%")
+    }
+  } else {
+    if(is.null(abDel)){
+      abDel <- abAmp
+      labelAmp <- labelDel <- paste0(abAmp * 100, "%")
+    } else {
+      labelAmp <- paste0(abAmp * 100, "%")
+      labelDel <- paste0(abDel * 100, "%")
+    }
+  }
+  
+  pp <- getDefaultPlotParams(plot.type=2)
+  
+  #Change the ideogramheight param to create thicker ideograms 
+  pp$data1outmargin <- 150
+  pp$data2outmargin <- 150
+  
+  kp <- plotKaryotype(genome = hg, plot.type = 2, plot.params = pp, #labels.plotter = NULL,
+                      chromosomes = paste0("chr", c("Y", "X", 22:1)))
+  
+  #kpText(kp, chr = paste0("chr", c(1:22, "X", "Y")), y=y.pos, x=chrom.length, r0=0, r1=2, 
+  #       labels=c(1:22, "X", "Y"), cex=1, pos=4, srt=90)
+  
+  
+  kpBars(kp, chr=bedAmp$chr, x0=bedAmp$start, x1=bedAmp$end, y1=bedAmp$fry, r0 = 0, r1=2,
+         col = colorAmp, border = NA, data.panel = 1)
+  kpBars(kp, chr=bedDel$chr, x0=bedDel$start, x1=bedDel$end, y1=bedDel$fry, r0 = 0, r1=2, data.panel = 2,
+         col = colorDel, border = NA)
+  
+  if(includeAB){
+    kpAbline(kp, r0=0, r1=2, h=abAmp, col = "grey", lty = 3)
+    kpText(kp, chr = paste0("chr", c(1:22, "X", "Y")), y=0.3, x=chrom.length, r0=0, r1=2, 
+           col="#444444", labels=labelAmp, cex=0.6, pos=4, srt=0)
+    
+    
+    kpAbline(kp, r0=0, r1=2, h=abDel, data.panel = 2, col = "grey", lty = 3)
+    kpText(kp, chr = paste0("chr", c(1:22, "X", "Y")), y=0.7, x=chrom.length, data.panel = 2, r0=0, r1=2, 
+           col="#444444", labels=labelDel, cex=0.6, pos=4, srt=0)
+  }
   
 }
